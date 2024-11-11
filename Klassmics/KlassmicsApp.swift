1203b5a4892cdbc0c8d33c07073c41290931b4ec
kevinumarov <keulifeinfluencer@gmail.com>
2024-11-11 20:38:00 +0900

    Initial Commit

---
+++ Klassmics/KlassmicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlassmicsApp.swift
+//  Klassmics
+//
+//  Created by Kevin Umarov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct KlassmicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
